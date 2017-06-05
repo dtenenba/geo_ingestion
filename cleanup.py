717d29560ccb2cc4e9b742e0420e86bd5d72085f
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-06-05 11:28:17 -0700

    add a warning

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,9 @@
 
 """
 Drop database in order to have a clean run.
+This should ONLY be run on a development database where
+you don't care if data is deleted. It DROPS
+the database and REMOVES ALL DATA in it!
 """
 
 import os
